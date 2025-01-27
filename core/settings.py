cdea18dc9ac59f94c58bdee9e3aebf0e7e785e9b
amirmohammadrezaee0090 <amirrezayi0101@gmail.com>
2025-01-27 16:29:24 +0330

    inisiate the todo and accounts

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'accounts.apps.AccountsConfig',
+    'todo.apps.TodoConfig'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = "/accounts/login"
---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from todo.views import *
+
+urlpatterns = [
+    path("", TaskList.as_view(), name="task_list"),
+    path("create/", TaskCreate.as_view(), name="create_task"),
+    path("update/<int:pk>/", TaskUpdate.as_view(), name="update_task"),
+    path("complete/<int:pk>/", TaskComplete.as_view(), name="complete_task"),
+    path("delete/<int:pk>/", DeleteView.as_view(), name="delete_task"),
+]
